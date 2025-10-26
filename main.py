3c3a895caa5806fedf755a734a8df6ed7f1d7cc5
Lucas Pires <lucas@checksec.com>
2025-10-26 22:09:31 +0000

    Add more endpoints

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException
+from fastapi.security import OAuth2PasswordBearer
 from sqlmodel import SQLModel, create_engine, Session, select
 from pydantic import BaseModel
 from typing import Annotated
@@ -13,6 +14,7 @@ class AmiiboSeries(BaseModel):
     id: int
     name: str
 
+
 class Amiibo(BaseModel):
     id: int
     name: str
@@ -24,8 +26,9 @@ class User(BaseModel):
     id: int
     full_name: str
     email: str
-    password: str
-    amiibos: list[Amiibo]
+    enabled: bool = True
+    owned_amiibos: list[Amiibo]
+    wanted_amiibos: list[Amiibo]
 
 
 def create_db():
@@ -39,8 +42,17 @@ def get_db_session():
 
 SessionDep = Annotated[Session, Depends(get_db_session)]
 
-app = FastAPI()
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
+TokenDep = Annotated[str, Depends(oauth2_scheme)]
+
+def convert_token_to_user(token: str) -> User:
+    return User(id=1, full_name="John Doe", email="john@example.com")
+
+def get_current_user(token: TokenDep) -> User:
+    return convert_token_to_user(token)
+
+app = FastAPI()
 
 @app.on_event("startup")
 def startup():
@@ -50,12 +62,14 @@ def startup():
 def amiibo_list(session: SessionDep) -> list[Amiibo]:
     return session.exec(select(Amiibo)).all()
 
+
 @app.get("/amiibo/{id}")
-def amiibo_get(id: int, session: SessionDep) -> Amiibo:
+def amiibo_detail(id: int, session: SessionDep) -> Amiibo:
     amiibo = session.get(Amiibo, id)
     if not amiibo:
         raise HTTPException(status_code=404, detail="Amiibo not found")
     return amiibo
+
 
 def amiibo_create():
     pass
@@ -65,5 +79,11 @@ def amiibo_update(id: int):
     pass
 
 
-def amiibo_delete(id: int):
-    pass
+@app.delete("/amiibo/{id}")
+def amiibo_delete(id: int, session: SessionDep) -> None:
+    amiibo = session.get(Amiibo, id)
+    if not amiibo:
+        raise HTTPException(status_code=404, detail="Amiibo not found")
+    session.delete(amiibo)
+    session.commit()
+    return None
